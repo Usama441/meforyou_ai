f7bf49fd2128d834781b0ff12847652fe556accf
Usama441 <usamaarshad441@gmail.com>
2025-06-29 17:54:56 +0500

    favicon add

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -6,3 +6,7 @@
 //= link_tree ../../../vendor/javascript .js
 //= link chat_stream.js
 //= link logo.png
+
+//= link_directory ../images/favicons .png
+//= link favicons/favicon.ico
+//= link favicons/site.webmanifest
