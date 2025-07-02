84cb55326ab2b3dd6c0ba4fc989878109efb9117
Usama441 <usamaarshad441@gmail.com>
2025-07-02 19:00:47 +0500

    working on signup form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_02_022955) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_02_120157) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -78,6 +78,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_02_022955) do
     t.string "name"
     t.string "ai_name"
     t.string "person_name"
+    t.string "first_name"
+    t.string "last_name"
+    t.date "dob"
+    t.string "gender"
+    t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
