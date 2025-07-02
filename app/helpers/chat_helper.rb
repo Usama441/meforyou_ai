dacacf60c94b17907102185ce62fdfab4f728e15
Usama441 <usamaarshad441@gmail.com>
2025-07-02 09:17:28 +0500

    redis basic setting

--- app/helpers/chat_helper.rb
+++ app/helpers/chat_helper.rb
@@ -1,2 +1,26 @@
 module ChatHelper
+  def relationship_options
+    [
+      "Mother",
+      "Father",
+      "Brother",
+      "Sister",
+      "Friend",
+      "Male Friend",
+      "Female Friend",
+      "Partner",
+      "Ex-Partner",
+      "Cousin",
+      "Uncle",
+      "Aunt",
+      "Chachu",
+      "Teacher",
+      "Boss",
+      "Colleague",
+      "Child",
+      "Mentor",
+      "AI"
+    ]
+  end
+
 end
