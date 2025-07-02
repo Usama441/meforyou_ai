c58cee4b989cf85f42a305e61ce1edd3107b8b7c
Usama441 <usamaarshad441@gmail.com>
2025-07-02 21:45:56 +0500

    smaller frontend issue resolved

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,26 +5,21 @@ class User < ApplicationRecord
 
   attr_accessor :contact_input
 
-  # Associations (if any)
+  # Associations
   has_many :chats, dependent: :destroy
   has_many :chat_sessions
   has_many :conversations, dependent: :destroy
-  # Validations
-  validates :relationship, presence: true
-  validates :name, presence: true
-  validates :ai_name, presence: true
-  validate  :validate_contact_input
-  before_validation :assign_contact_input
-  GLOBAL_PHONE_REGEX = /\A\+?[1-9]\d{1,14}\z/
 
-  validate :validate_global_phone
+  # Validations
+  validate :validate_contact_input
+  before_validation :assign_contact_input
 
   def assign_contact_input
     return if contact_input.blank?
 
     if contact_input =~ URI::MailTo::EMAIL_REGEXP
       self.email = contact_input
-    elsif contact_input =~ /\A(\+92|0)?3[0-9]{9}\z/ || contact_input =~ /\A\+?[1-9]\d{1,14}\z/
+    elsif contact_input =~ /\A(\+92|0)?3[0-9]{9}\z/ || contact_input =~ /\A\+?[1-9]\d{9,14}\z/
       self.phone = normalize_phone(contact_input)
     else
       errors.add(:contact_input, "must be a valid email or phone number")
@@ -37,10 +32,18 @@ class User < ApplicationRecord
     end
   end
 
-  def validate_global_phone
-    if contact_input.present? && !(contact_input =~ GLOBAL_PHONE_REGEX)
-      errors.add(:contact_input, "must be a valid international phone number")
+  def validate_contact_input
+    input = contact_input.to_s.strip
+    if input.blank?
+      errors.add(:contact_input, "can't be blank")
+    elsif input.include?("@")
+      unless input =~ URI::MailTo::EMAIL_REGEXP
+        errors.add(:contact_input, "must be a valid email")
+      end
+    else
+      unless input.match?(/\A(\+92|0)?3[0-9]{9}\z/) || input.match?(/\A\+?[1-9]\d{9,14}\z/)
+        errors.add(:contact_input, "must be a valid phone number")
+      end
     end
   end
 end
-
