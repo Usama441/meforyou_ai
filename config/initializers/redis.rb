29ab8e64b1d2d0745af72f4b13a6cf464a19a9ad
Usama441 <usamaarshad441@gmail.com>
2025-07-04 03:53:51 +0500

    redis url

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,19 @@
+#require 'redis'
+
+#$redis = Redis.new(
+#  host: 'localhost',
+#  port: 6379,
+#  db: 0
+#)
+
 require 'redis'
 
-$redis = Redis.new(
-  host: 'localhost',
-  port: 6379,
-  db: 0
-)
+if ENV["REDIS_URL"]
+  $redis = Redis.new(url: ENV["REDIS_URL"])
+else
+  $redis = Redis.new(
+    host: 'localhost',
+    port: 6379,
+    db: 0
+  )
+end
