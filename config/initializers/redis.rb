89a306426f8d320aeaa7da3585be8503d5777f32
Usama441 <usamaarshad441@gmail.com>
2025-07-04 05:11:17 +0500

    redis solve 0.6

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -9,8 +9,15 @@
 require 'redis'
 
 redis_url = ENV["REDIS_URL"] || "redis://localhost:6379/0"
-$redis = Redis.new(url: redis_url,
-                   reconnect_attempts: 1,
-                   connect_timeout: 5,
-                   read_timeout: 5,
-                   write_timeout: 5)
+begin
+$redis = Redis.new(
+  url: ENV.fetch("REDIS_URL"),
+  reconnect_attempts: 1,
+  connect_timeout: 5,
+  read_timeout: 5,
+  write_timeout: 5
+)
+
+rescue URI::InvalidURIError => e
+Rails.logger.error("Invalid REDIS_URL: #{e.message}")
+end
